1c0f5e941eeabb3dbf181541b4a96f63c243db5a
Denys <denys.hudymov@gmail.com>
2024-03-13 01:26:49 +0200

    main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { YouthPoliticalComponent } from './youth-political/youth-political.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuizComponent, HeaderComponent, FooterComponent, YouthPoliticalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Site_with_quiz';
+  title = 'YPS_Latvia';
 }
