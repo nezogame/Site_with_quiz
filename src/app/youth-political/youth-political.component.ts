78223d741689c512933325c6206e73826d9488fe
Denys <denys.hudymov@gmail.com>
2024-03-13 00:57:04 +0200

    i created component for page youth-political

---
+++ src/app/youth-political/youth-political.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-youth-political',
+  standalone: true,
+  imports: [],
+  templateUrl: './youth-political.component.html',
+  styleUrl: './youth-political.component.css'
+})
+export class YouthPoliticalComponent {
+
+}
