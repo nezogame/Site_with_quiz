3801e64a18e27e7cec354ae806fc367819c51d31
Denys <denys.hudymov@gmail.com>
2024-03-18 00:07:22 +0200

    I implemented workshops page with sended design and add some additional css styles

--- src/app/workshops/workshops.component.ts
+++ src/app/workshops/workshops.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { SpeakerInfoComponent } from '../speaker-info/speaker-info.component';
 
 @Component({
   selector: 'app-workshops',
   standalone: true,
-  imports: [],
+  imports: [SpeakerInfoComponent],
   templateUrl: './workshops.component.html',
   styleUrl: './workshops.component.css'
 })
 export class WorkshopsComponent {
 
+  constructor() {
+  }
+
+  selectedElement: String = "thirdWorkshop";
+
+  selectWorkshop(workshop: string) {
+    this.selectedElement = workshop;
+  }
 }
